84c9d1222d9a5f6dfde339994c95e9db4707820f
nona-git <ikazuchi.1720@gmail.com>
2023-12-27 16:32:46 +0900

    test

--- example.py
+++ example.py
@@ -83,7 +83,7 @@ async def gemusen(interaction: discord.Interaction, member: Optional[discord.Mem
 
 @client.tree.command()
 @app_commands.describe(
-    first_value='The first value you want to add something to',
+    first_value='The first value you want to add something to ',
     second_value='The value you want to add to the first value',
 )
 async def add(interaction: discord.Interaction, first_value: int, second_value: int):
